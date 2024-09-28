caa7718130bb3965817b5bc900e84251198c3b5c
CODE ME <cscodepractice@gmail.com>
2024-09-28 10:25:48 +0530

    init

---
+++ Entity_framework/Entity_framework/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Entity_framework.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Student data)
+        {
+            using (var DbContext = new mvcEntity())
+            {
+                var entity
+            }
+                return View();
+        }
+    }
+}
---
+++ Entity_framework/Entity_framework/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entity_framework.Models
+{
+    public class Student
+    {
+        public int name{ get; set; } 
+        public int age { get; set; }
+        public string phone { get; set; }
+    }
+}
